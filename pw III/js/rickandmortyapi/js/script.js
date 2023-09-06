c77073d343730030b22762f15bb431cb6073adf3
Miguel de Oliveira Santos <miguel.santos6700@gmail.com>
2023-09-06 08:27:50 -0300

    Refatorado código

--- pw III/js/rickandmortyapi/js/script.js
+++ pw III/js/rickandmortyapi/js/script.js
@@ -1,54 +1,58 @@
 const url = `https://rickandmortyapi.com/api/character`
-
-
 const listCharacters = document.querySelector('#characters')
 
-fetch(url)
-.then(response => response.json())
-.then(data => {
+
+getCharacters()
+
+function getCharacters() {
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
     
-    data.results.forEach(character => {
-        const characterElement = document.createElement('div')
-        characterElement.className = 'character'
-        listCharacters.appendChild(characterElement)
+      data.results.forEach(character => {
+            const characterElement = document.createElement('div')
+            characterElement.className = 'character'
+            listCharacters.appendChild(characterElement)
 
-        console.log(character.status)
+            let status
+            let gender
+            let species
 
-        let status
-        let gender
-        let species
+            if (character.species == "Human") {
+                species = "Humano"
+            } else {
+                species = "Alienigena"
+            }
 
-        if (character.species == "Human") {
-            species = "Humano"
-        } else {
-            species = "Alienigena"
-        }
+            if (character.status == "Alive") {
+                status = "Vivo (a)"
+            } else if (character.status == "Dead"){
+                status = "Morto (a)    "
+            } else {
+                status = "Status de vida desconhecido"
+            }
 
-        if (character.status == "Alive") {
-            status = "Vivo (a)"
-        } else if (character.status == "Dead"){
-            status = "Morto (a)    "
-        } else {
-            status = "Status de vida desconhecido"
-        }
+            if (character.gender == "Male") {
+                gender = "Homem"
+            } else {
+                gender = "Mulher"
+            }
 
-        if (character.gender == "Male") {
-            gender = "Homem"
-        } else {
-            gender = "Mulher"
-        }
+            characterElement.innerHTML = 
+            `
+            <img src=${character.image}
+            <p> ${character.name} </p>
+            <p> ${gender} </p>
+            <p> ${species} </p>
+            <p> ${status} </p>
+            `
 
-        characterElement.innerHTML = 
-        `
-        <img src=${character.image}
-        <p> ${character.name} </p>
-        <p> ${gender} </p>
-        <p> ${species} </p>
-        <p> ${status} </p>
-
-        `
-
-    })
-}).catch(error => {
+        })
+    }).catch(error => {
     console.error(`erro na api: ${error}`)
 })
+
+}
+
+
