544f4b9957a6e41f3c0a3e6adf0c78c2ddb9cfe4
Your Name <you@example.com>
2023-09-26 10:51:38 -0300

    Feito requisição da API

--- pam III/cinehub/src/components/cardFilmes/index.js
+++ pam III/cinehub/src/components/cardFilmes/index.js
@@ -3,19 +3,20 @@ import style from "./style";
 import {Image, Text, TouchableOpacity} from 'react-native';
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
 
-export default function Card(filme){
+export default function Card({ nome, nota, imagem }){
 
     const Navigation = useNavigation();
 
     return(
 
         <TouchableOpacity style = {style.containerFilmes}
-            onPress={()=> Navigation.navigate('details',{titulo:filme.nome, nota:filme.nota, imagem:filme.imagem})}
+            onPress={()=> Navigation.navigate('details')}
         >
 
-            <Image style = {style.imagemFilmes} source = {require(`../../img/${filme.imagem}`)} />
-            <Text style = {style.tituloFilmes} >{filme.nome}</Text>
-            <Text style = {style.notaFilmes} >{filme.nota}</Text>
+        <Image style={style.imagemFilmes} source = {{uri:`https://image.tmdb.org/t/p/original/${imagem}` }} />    
+
+            <Text style = {style.tituloFilmes} >{nome}</Text>
+            <Text style = {style.notaFilmes} >{nota}</Text>
 
         </TouchableOpacity>
 
--- pam III/cinehub/src/screens/Home/index.js
+++ pam III/cinehub/src/screens/Home/index.js
@@ -9,10 +9,57 @@ import Card from '../../components/cardFilmes';
 import filmes from '../../data/movies';
 import series from '../../data/series';
 import titulo from '../../components/bannerFilmes/style';
+import React,{useState, useEffect} from 'react';
+
 
 export default function App() {
 
   document.title = 'CineDev';
+
+  const [movies, setMovies] = useState([]);
+  const [series, setSeries] = useState([]);
+
+  useEffect(() => {
+    async function getMovies() {
+      try{
+      const response = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=fd64a3925e5bdd7e06bf0547cf14e4f8&language=pt-br');
+        const data = await response.json();
+
+        console.log(data.results)
+        console.log(data.results[0].title)
+        console.log(data.results[0].poster_path)
+        setMovies(data.results)
+
+      }
+      
+      catch(error){
+        console.log("requisição falhou", error)
+      }
+    
+    }
+    getMovies();
+  }, [])
+
+
+  useEffect(() => {
+    async function getSeries() {
+      try{
+      const response = await fetch(
+        'https://api.themoviedb.org/3/tv/popular?api_key=fd64a3925e5bdd7e06bf0547cf14e4f8&language=pt-br');
+        const data = await response.json();
+
+        console.log(data.results)
+        setSeries(data.results)
+
+      }
+      
+      catch(error){
+        console.log("requisição falhou", error)
+      }
+    
+    }
+    getSeries();
+  }, [])
 
   return (
     <View style={style.container}>
@@ -24,24 +71,22 @@ export default function App() {
 
       <Text style={titulo.textoBanner}>Filmes</Text>
       <View style ={{width: '90%'}}>
-        <FlatList
-          horizontal = {true}
-          showsHorizontalScrollIndicator = {false}
-          data = {filmes}
-          keyExtractor = {(item) => item.id}
-          renderItem = { ({item}) => (
-
-            <Card
+      <FlatList
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            data={movies}
+            keyExtractor={(item) => item.id}
             
-              nome = {item.nome}
-              nota = {item.nota}
-              imagem = {item.imagem}
             
-            />
-
-          )}
-        
-        />
+            renderItem={({ item }) => (
+              
+              <Card
+              imagem={item.poster_path}
+              nome={item.title}
+              nota={item.vote_average}
+              />
+              )}
+          />
       </View>  
 
       <Text style={titulo.textoBanner}>Séries</Text>
@@ -55,9 +100,9 @@ export default function App() {
 
             <Card
             
-              nome = {item.nome}
-              nota = {item.nota}
-              imagem = {item.imagem}
+              imagem = {item.poster_path}
+              nome = {item.name}
+              nota = {item.vote_average}
             
             />
 
@@ -65,6 +110,8 @@ export default function App() {
         
         />
       </View>  
+
+     
 
     </View>
   );
